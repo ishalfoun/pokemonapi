cccf4f466586fce46c3ef44a38e4baf9a6311107
i s <isaak.shalfoun@gmail.com>
2022-07-26 15:38:41 +0200

    feat(hosting): implement firbase hosting

---
+++ pokemonapi/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAh8Q76hLIaowZkMHWk1EQBVhixTO786F8",
+  authDomain: "a9c965.firebaseapp.com",
+  projectId: "a9c965",
+  storageBucket: "a9c965.appspot.com",
+  messagingSenderId: "380534221641",
+  appId: "1:380534221641:web:2a2d9d75297aee33c04898"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
